3de7611fb3730dd5f66d6636ef1db1aac4e5cb03
clia <clia@163.com>
2022-07-14 14:11:23 +0800

    Disable nginx

--- src/nginx.rs
+++ src/nginx.rs
@@ -1,15 +1,15 @@
-use std::io::Read;
-use std::fs::File;
+// use std::io::Read;
+// use std::fs::File;
 
-use nginx_config::parse_main;
+// use nginx_config::parse_main;
 
-pub fn read_config() {
-    let mut buf = String::with_capacity(1024);
-    let path = format!("conf/default.conf");
-    let mut f = File::open(&path).unwrap();
-    f.read_to_string(&mut buf).unwrap();
-    // println!("{}", buf);
-    let ast = parse_main(&buf).unwrap();
-    println!("{}", ast);
-    // assert_eq!(ast.to_string(), buf);
-}
+// pub fn read_config() {
+//     let mut buf = String::with_capacity(1024);
+//     let path = format!("conf/default.conf");
+//     let mut f = File::open(&path).unwrap();
+//     f.read_to_string(&mut buf).unwrap();
+//     // println!("{}", buf);
+//     let ast = parse_main(&buf).unwrap();
+//     println!("{}", ast);
+//     // assert_eq!(ast.to_string(), buf);
+// }
